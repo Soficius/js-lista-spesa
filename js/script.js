dc355aec01a66c555f28c694fe8eeb3609e58aa6
Soficius <103771506+Soficius@users.noreply.github.com>
2022-06-28 17:19:51 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// Descrizione:
+// Rivediamo l’esercizio della lista della spesa visto insieme, ma questa volta con il ciclo while al posto del ciclo for. Facciamo attenzione a non dimenticare tutte le differenze implementative che il ciclo while ci impone, in particolare alla variabile di indice.
+// Consigli:
+// Potete provare a fare l'esercizio una prima volta col for e poi convertire piano piano in while.
+// Lista della spesa (da convertire in array)
+// Uova
+// Pane
+// Latte
+// Biscotti
+// Pasta
+// Affettati
+// Formaggio
+// Verdura
+// Frutta
+
+const spesa = ['Uova', 'Pane', 'Latte', 'Biscotti', 'Pasta', 'Affettati', 'formaggio', 'verdura', 'frutta'];
+let result
+for(let i = 0; i < spesa.length; i++){
+    
+}
