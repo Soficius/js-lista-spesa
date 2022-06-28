81d46c65ccdaa73f9efc0ff600cde9551759077a
Soficius <103771506+Soficius@users.noreply.github.com>
2022-06-28 17:52:14 +0200

    fixed error

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 const list = document.getElementById('list')
 const spesa = ['Uova', 'Pane', 'Latte', 'Biscotti', 'Pasta', 'Affettati', 'formaggio', 'verdura', 'frutta'];
-let lista = ''
 
 //* ciclo con for
 // for(let i = 0; i < spesa.length; i++){
 //     lista += `<li>${spesa[i]}</li>`
 // }
-
+    
+let lista = ''
 let i = 0
 while(i < spesa.length){
     lista += `<li>${spesa[i]}</li>`
